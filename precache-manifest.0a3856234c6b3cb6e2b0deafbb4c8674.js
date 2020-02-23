37a81a2392f8a801ddd07a3a4f6f5ed9fbb0ec7b
Alvin Hager <alvinhager@Alvins-Air.lan>
2020-02-23 21:40:32 +0100

    Updates

---
+++ precache-manifest.0a3856234c6b3cb6e2b0deafbb4c8674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7a4019776dc9de585cf4974690eefd",
+    "url": "/IVISProject2AlvinHager/index.html"
+  },
+  {
+    "revision": "20ac1c3d0955fa2470c3",
+    "url": "/IVISProject2AlvinHager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "54fe45f54b2122547bcb",
+    "url": "/IVISProject2AlvinHager/static/css/main.2af7bc83.chunk.css"
+  },
+  {
+    "revision": "20ac1c3d0955fa2470c3",
+    "url": "/IVISProject2AlvinHager/static/js/2.277087f8.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/IVISProject2AlvinHager/static/js/2.277087f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fe45f54b2122547bcb",
+    "url": "/IVISProject2AlvinHager/static/js/main.2563419f.chunk.js"
+  },
+  {
+    "revision": "a30e5ecd8031f9a2368d",
+    "url": "/IVISProject2AlvinHager/static/js/runtime-main.1093bbbd.js"
+  }
+]);
